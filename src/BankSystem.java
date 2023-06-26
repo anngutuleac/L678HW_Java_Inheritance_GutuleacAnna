c3e8a903bc9721449cbae97d6cc472621fe803d6
Ann Gutuleac <ann.gutuleac@gmail.com>
2023-06-26 13:06:12 +0300

    Add BankSystem class

---
+++ src/BankSystem.java
@@ -0,0 +1,116 @@
+public class BankSystem {
+    public static void main(String args[]) {
+        int clientID = 100001;
+        String clientName = "Anna Gutuleac";
+        int savingsCountID = 200002;
+        double sum = 100;
+        String currency = "USD";
+        int transactionID = 300003;
+
+        SavingsCount sc = createSavingsCount(savingsCountID, sum, currency);
+        Client client = createClient(clientID, clientName, sc);
+        Transaction transaction = createTransaction(transactionID, client);
+
+        countReport(client);
+        transaction.addMoney(200);
+        countReport(client);
+        transaction.extractMoney(150);
+        countReport(client);
+        transaction.extractMoney(350);
+        countReport(client);
+    }
+    private static CurrentCount createCurrentCount(int id, double sum, String currency) {
+        return new CurrentCount(id, sum, currency);
+    }
+
+    private static SavingsCount createSavingsCount(int id, double sum, String currency) {
+        return new SavingsCount(id, sum, currency);
+    }
+
+    private static Client createClient(int id, String name, Count count) {
+        return new Client(id, name, count);
+    }
+
+    private static Transaction createTransaction(int id, Client client) {
+        return new Transaction(id, client);
+    }
+
+    private static void countReport(Client client) {
+        System.out.println("Клиент: " + client.name);
+        System.out.println("Тип счета: " + client.count.type);
+        System.out.println("Состояние счета " + client.count.id + ": " + client.count.sum + " " + client.count.currency);
+    }
+}
+
+class Client {
+    int id;
+    String name;
+    Count count;
+
+    public Client(int id, String name, Count count) throws IllegalArgumentException {
+        if (id <= 0) { throw new IllegalArgumentException("ID клиента должен быть положительным!"); }
+        if (name.length() == 0) { throw new IllegalArgumentException("Имя клиента не должно быть пустым!"); }
+        this.id = id;
+        this.name = name;
+        this.count = count;
+    }
+}
+
+class Count {
+    int id;
+    String type;
+    double sum;
+    String currency;
+
+    public Count(int id, String type, double sum, String currency) throws IllegalArgumentException {
+        if (id <= 0) { throw new IllegalArgumentException("ID счета должен быть положительным!"); }
+        if (type.length() == 0) { throw new IllegalArgumentException("Тип счета должна иметь валидное значение!"); }
+        if (sum < 0) { throw new IllegalArgumentException("Сумма на счету должна быть неотрицательной!"); }
+        if (currency.length() == 0) { throw new IllegalArgumentException("Валюта должна иметь валидное значение!"); }
+        this.id = id;
+        this.type = type;
+        this.sum = sum;
+        this.currency = currency;
+    }
+}
+
+class CurrentCount extends Count {
+    public CurrentCount(int id, double sum, String currency) throws IllegalArgumentException {
+        super(id, "Текущий", sum, currency);
+    }
+}
+
+class SavingsCount extends Count {
+    public SavingsCount(int id, double sum, String currency) throws IllegalArgumentException {
+        super(id, "Накопительный", sum, currency);
+    }
+}
+
+class Transaction {
+    int id;
+    Client client;
+
+    public Transaction(int id, Client client) throws IllegalArgumentException {
+        if (id <= 0) { throw new IllegalArgumentException("ID транзакции должен быть положительным!"); }
+        this.id = id;
+        this.client = client;
+    }
+
+    void addMoney(double sum) {
+        if (sum < 0) {
+            System.out.println("Сумма пополнения счета должна быть положительной!");
+        } else {
+            client.count.sum += sum;
+            System.out.println("Было добавлено: " + sum);
+        }
+    }
+
+    void extractMoney(double sum) {
+        if (client.count.sum < sum) {
+            System.out.println("У вас недостаточно средств на счету!");
+        } else {
+            client.count.sum -= sum;
+            System.out.println("Было снято: " + sum);
+        }
+    }
+}
---
+++ src/QDemo2.java
@@ -0,0 +1,85 @@
+// Класс, реализующий очередь для хранения символов
+class Queue {
+    private char q[]; // массив для хранения элементов очереди
+    private int putloc, getloc; // индексы для вставки и
+
+    // извлечения элементов очереди
+    Queue(int size) {
+        q = new char[size + 1];  // выделить память для очереди
+        putloc = getloc = 0;
+    }
+
+    // Сконструировать очередь на основе имеющегося объекта Queue
+    Queue(Queue ob) {
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+// Копировать элементы в очередь
+        for (int i = getloc + 1; i <= putloc; i++)
+            q[i] = ob.q[i];
+    }
+
+    // Сконструировать очередь на основе массива исходных значений
+    Queue(char a[]) {
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length + 1];
+        for (int i = 0; i < a.length; i++) put(a[i]);
+    }
+
+    // Поместить символ в очередь
+    void put(char ch) {
+        if (putloc == q.length - 1) {
+            System.out.println(" - Очередь заполнена");
+            return;
+        }
+        putloc++;
+        q[putloc] = ch;
+    }
+
+    // Извлечь символ из очереди
+    char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Очередь пуста");
+            return (char) 0;
+        }
+        getloc++;
+        return q[getloc];
+    }
+}
+
+// Продемонстрировать использование класса Queue
+class QDemo2 {
+    public static void main(String args[]) {
+// Создать пустую очередь для хранения 10 элементов
+        Queue q1 = new Queue(10);
+        char name[] = {'Т', 'о', 'm'};
+// Создать очередь на основе массива
+        Queue q2 = new Queue(name);
+        char ch;
+        int i;
+// Поместить ряд символов в очередь q1
+        for (i = 0; i < 10; i++)
+            q1.put((char) ('A' + i));
+// Создать одну очередь на основе другой
+        Queue q3 = new Queue(q1);
+// Показать очереди
+        System.out.print("Coдepжимoe q1: ");
+        for (i = 0; i < 10; i++) {
+            ch = q1.get();
+            System.out.print(ch);
+        }
+        System.out.println("\n");
+        System.out.print("Coдepжимoe q2: ");
+        for (i = 0; i < 3; i++) {
+            ch = q2.get();
+            System.out.print(ch);
+            System.out.println("\n");
+            System.out.print("Coдepжимoe qЗ: ");
+            for (i = 0; i < 10; i++) {
+                ch = q3.get();
+                System.out.print(ch);
+            }
+        }
+    }
+}
